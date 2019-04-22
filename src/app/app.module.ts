f3bbf08d06fb8f39702a41579fce0e2788283a0f
Ankmap <mapariit@gmail.com>
2019-04-22 15:48:19 +0530

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 /**
  * @Purpose : Configure animations
- * import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
  **/ 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+/**
+ * @Purpose : Router imports
+ **/ 
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
@@ -13,6 +15,18 @@ import { RegistrationComponent } from './component/registration/registration.com
 import { ForgotPasswordComponent } from './component/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './component/reset-password/reset-password.component';
 import { from } from 'rxjs';
+/**
+ * @Purpose : Material Required file
+ **/
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule } from '@angular/material'; 
+import {MatMenuModule} from '@angular/material/menu';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'forgotPassword', component: ForgotPasswordComponent },
+  { path: 'resetPassword', component: ResetPasswordComponent },
+];
 
 @NgModule({
   declarations: [
@@ -25,8 +39,25 @@ import { from } from 'rxjs';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatMenuModule
   ],
+
+  exports: [
+    RouterModule
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
