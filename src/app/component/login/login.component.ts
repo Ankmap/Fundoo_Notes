5e33b4534ffa7f6b3972cea634f610756da70e35
Ankmap <mapariit@gmail.com>
2019-04-30 12:27:21 +0530

    ok

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormControl,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,13 +10,12 @@ export class LoginComponent implements OnInit {
 
   constructor() { }
 
-  email = new FormControl('', [Validators.required, Validators.email]);
+  email = new FormControl('', [Validators.required, Validators.email,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]);
   getErrorMessage() {
     return this.email.hasError('required') ? 'You must enter a value' :
-      this.email.hasError('email') ? 'Not a valid email' :
-        '';
+      this.email.hasError('email') ? 'Not a valid email' :'';
   }
-
+// ngOnInit function is called whenever the component is loaded. And used to load the data for the component
   ngOnInit() {
   }
 
--- src/app/component/login1/login1.component.ts
+++ src/app/component/login1/login1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login1',
@@ -8,8 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class Login1Component implements OnInit {
 
   constructor() { }
+  email = new FormControl('', [Validators.required, Validators.email,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]);
 
   ngOnInit() {
   }
-
+  getErrorMessage(){
+    return this.email.hasError('required') ? 'You must enter a value' :
+    this.email.hasError('email') ? 'Not a valid email' :'';
+  }
 }
