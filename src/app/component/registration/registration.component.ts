90d52efa288226143a48c7a6b528f9109b7866d1
Ankmap <mapariit@gmail.com>
2019-05-02 09:49:52 +0530

    register ok

--- src/app/component/registration/registration.component.ts
+++ src/app/component/registration/registration.component.ts
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
-  register: User = new User();  
+  register: User = new User();
   service: any;
   constructor(private userService: UserserviceService, private snackbar: MatSnackBar, private router: Router) {
     //Button
@@ -22,6 +22,8 @@ export class RegistrationComponent implements OnInit {
   email = new FormControl('', [Validators.required, Validators.email, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]);
   password = new FormControl('', [Validators.required, Validators.minLength(4)]);
   confirmPassword = new FormControl('', [Validators.required, Validators.minLength(4)])
+  advancedService = new FormControl('', [Validators.required]);
+  basicService = new FormControl('', [Validators.required]);
   firstNameValidation() {
     return this.firstName.hasError('required') ? 'You must enter a firstName' :
       this.firstName.hasError('firstName') ? 'Not a valid firstName' : '';
@@ -42,6 +44,14 @@ export class RegistrationComponent implements OnInit {
     return this.confirmPassword.hasError('required') ? 'You must enter a confirmPassword' :
       this.confirmPassword.hasError('confirmPassword') ? 'Enter min 4 digit confirmPassword' : '';
   }
+  advancedServiceValidation() {
+    return this.advancedService.hasError('required') ? 'You must choose a value' : '';
+    // this.advancedService.hasError('advancedService') ? 'Enter min 4 digit confirmPassword' : '';
+  }
+  basicServiceValidation() {
+    return this.basicService.hasError('required') ? 'You must choose a value' : '';
+    // this.basicService.hasError('basicService') ? 'Enter min 4 digit confirmPassword' : '';
+  }
   //Button
   currentLesson: string;
 
@@ -57,24 +67,33 @@ export class RegistrationComponent implements OnInit {
   }
 
   submit() {
-    console.log('console @@@@@@@@@@@@@@@@@=======================>', this.register);
+    console.log('console forthis.register @@@@@@@@@@@@@@@@@=======================>', this.register);
     try {
-      if (this.password.value != this.confirmPassword.value) 
-      throw "password and confirmpassword does not match"
-      // if(this.firstName.value == '' || this.lastName.value == '' || this.email.value == '' || this.service.value=='' || this.password.value == '' || this.confirmPassword.value == '') throw "Fields are missing"
+      if (this.password.value == this.confirmPassword.value)
+        // if (this.firstName.value == '' || this.lastName.value == ''
+        //   || this.email.value == '' || this.service.value == ''
+        //   || this.password.value == '' || this.confirmPassword.value == '')
+        //throw "Fields are missing"
+        // console.log("password and confirmpassword does not match");
+        // this.snackbar.open('password and confirmpassword does not match......!', 'Error...!', { duration: 1000 });
 
-      this.userService.postRequest('user/userSignUp', this.register).subscribe(
-        data => {
-          console.log("console =======================>", data);
-          this.snackbar.open('Register successfully......!', 'Done...!', { duration: 1000 });
-          this.router.navigateByUrl('login');
-        },
-        error => {
-          this.snackbar.open('Register not successfully......!', 'Stop...!', { duration: 3000 });
-          console.log("Error something wrong: ", error)
-        });
+        this.userService.postRequest('user/userSignUp', this.register).subscribe(
+          data => {
+            console.log("console for data =======================>", data);
+            this.snackbar.open('Register done successfully......!', 'Done...!', { duration: 1000 });
+            this.router.navigateByUrl('login');
+            console.log('Register infor ==========>', data);
+          },
+          error => {
+            this.snackbar.open('Error while creating account......!', 'Error', { duration: 3000 });
+            console.log("Error something wrong: ", error)
+          });
+      else {
+        this.snackbar.open('password and confirmpassword does not match......!', 'Error...!', { duration: 1000 });
+      }
     } catch (error) {
-      this.snackbar.open('error', "", { duration: 3000 });
+      this.snackbar.open('error', "Must enter the all filed..!", { duration: 3000 });
+      console.log("password and confirmpassword does not match");
     }
   }
 }
